ff6971b82e5e4becb543323ff3330294ecdc5bc5
Killian Talin <killian.talin@live.fr>
2017-08-10 09:04:54 +0200

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "06 10 10 20 20",
+    category:     "French"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "06 17 17 17 17",
+    category:     "French"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "01 45 38 83 23",
+    category:     "Italian"
+  },
+  {
+    name:         "Esprit Tchai",
+    address:      "4 rue Oberkampf 75005 Paris",
+    phone_number: "06 20 82 23 02",
+    category:     "French"
+  },
+  {
+    name:         "L'Alicheur",
+    address:      "144 rue Oberkampf 75005 Paris",
+    phone_number: "01 34 43 34 04",
+    category:     "Chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
